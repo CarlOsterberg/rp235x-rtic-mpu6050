a2adf0e7d22ece6b424c61dd4fe59290dd643b19
Carl Österberg <36886642+CarlOsterberg@users.noreply.github.com>
2026-02-22 12:10:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Hello world on rp pico 235x
+
+This program writes "Hello RTIC UART" over UART on Gpio0/pin1.
+The UART settings are: baud rate 115200, 8 bits, no parity and 1 stop bit.
+
+### Installing dependencies
+
+```sh
+rustup target install thumbv8m.main-none-eabihf
+cargo install flip-link
+cargo install probe-rs-tools # If debugging with SWD
+```
+
+## Flashing and running the code
+
+There are two ways, either you can use the picotool or probe-rs. The pciotool
+requires less tools but you need to put the device into mass storage mode somehow.
+probe-rs is easier to use but you need a debugger supporting swd and to solder the debug pins.
+For details see the [cargo config](.cargo/config.toml).
+
+## License
+
+The contents of this repository are dual-licensed under the _[MIT](LICENSE-MIT) OR [Apache-2.0](LICENSE-APACHE-2.0)_ License. That means you can chose either the MIT licence or the
+Apache-2.0 licence when you re-use this code. See [`LICENSE-MIT`](LICENSE-MIT) or [`LICENSE-APACHE-2.0`](LICENSE-APACHE-2.0) for more
+information on each specific licence.
---
+++ src/main.rs
@@ -0,0 +1,120 @@
+#![no_std]
+#![no_main]
+
+use defmt_rtt as _;
+use fugit::RateExtU32;
+use panic_probe as _;
+use rp235x_hal::{
+    self as hal, Clock,
+    clocks::init_clocks_and_plls,
+    sio::Sio,
+    uart::{DataBits, StopBits, UartConfig},
+    watchdog::Watchdog,
+};
+use rtic_monotonics::rp235x::prelude::*;
+
+rp235x_timer_monotonic!(Mono);
+
+/// Tell the Boot ROM about our application
+#[unsafe(link_section = ".start_block")]
+#[used]
+pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
+
+/// Program metadata for `picotool info`
+#[unsafe(link_section = ".bi_entries")]
+#[used]
+pub static PICOTOOL_ENTRIES: [rp235x_hal::binary_info::EntryAddr; 5] = [
+    rp235x_hal::binary_info::rp_cargo_bin_name!(),
+    rp235x_hal::binary_info::rp_cargo_version!(),
+    rp235x_hal::binary_info::rp_program_description!(c"RP2350 RTIC hello world"),
+    rp235x_hal::binary_info::rp_cargo_homepage_url!(),
+    rp235x_hal::binary_info::rp_program_build_attribute!(),
+];
+
+#[rtic::app(device = rp235x_hal::pac, peripherals = true, dispatchers = [TIMER0_IRQ_1])]
+mod app {
+    use super::*;
+
+    type Uart0 = hal::uart::UartPeripheral<
+        hal::uart::Enabled,
+        hal::pac::UART0,
+        (
+            hal::gpio::Pin<hal::gpio::bank0::Gpio0, hal::gpio::FunctionUart, hal::gpio::PullDown>,
+            hal::gpio::Pin<hal::gpio::bank0::Gpio1, hal::gpio::FunctionUart, hal::gpio::PullDown>,
+        ),
+    >;
+
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {
+        uart: Uart0,
+    }
+
+    #[init]
+    fn init(mut ctx: init::Context) -> (Shared, Local) {
+        Mono::start(ctx.device.TIMER0, &mut ctx.device.RESETS);
+        // Configure the clocks, watchdog - The default is to generate a 125 MHz system clock
+        let mut watchdog = Watchdog::new(ctx.device.WATCHDOG);
+
+        // External high-speed crystal on the pico board is 12Mhz
+        let external_xtal_freq_hz = 12_000_000u32;
+        let clocks = init_clocks_and_plls(
+            external_xtal_freq_hz,
+            ctx.device.XOSC,
+            ctx.device.CLOCKS,
+            ctx.device.PLL_SYS,
+            ctx.device.PLL_USB,
+            &mut ctx.device.RESETS,
+            &mut watchdog,
+        )
+        .ok()
+        .unwrap();
+
+        let sio = Sio::new(ctx.device.SIO);
+
+        let pins = hal::gpio::Pins::new(
+            ctx.device.IO_BANK0,
+            ctx.device.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut ctx.device.RESETS,
+        );
+
+        let uart_pins = (
+            pins.gpio0.into_function::<hal::gpio::FunctionUart>(),
+            pins.gpio1.into_function::<hal::gpio::FunctionUart>(),
+        );
+
+        let baudrate: u32 = 115_200;
+
+        let uart =
+            hal::uart::UartPeripheral::new(ctx.device.UART0, uart_pins, &mut ctx.device.RESETS)
+                .enable(
+                    UartConfig::new(baudrate.Hz(), DataBits::Eight, None, StopBits::One),
+                    clocks.peripheral_clock.freq(),
+                )
+                .unwrap();
+
+        hello::spawn().ok();
+
+        (Shared {}, Local { uart })
+    }
+
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        loop {
+            cortex_m::asm::nop();
+        }
+    }
+
+    #[task(local = [uart], priority = 1)]
+    async fn hello(ctx: hello::Context) {
+        loop {
+            ctx.local.uart.write_full_blocking(b"Hello RTIC UART!\r\n");
+            Mono::delay(1000.millis()).await;
+        }
+    }
+}
+
+// End of file
