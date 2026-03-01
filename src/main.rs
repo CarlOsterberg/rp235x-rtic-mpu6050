80a5a564f5f801d6fb71318a98f5ae8f47560d42
Carl Österberg <carl.vilhelms.osterberg@gmail.com>
2026-03-01 01:05:15 +0100

    interrupt based MPU6050 sampling

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use core::fmt::Write;
 use defmt_rtt as _;
-use embedded_hal::i2c::{I2c};
+use embedded_hal::i2c::I2c;
 use fugit::RateExtU32;
 use heapless::String;
 use panic_probe as _;
@@ -55,6 +55,12 @@ mod app {
         ),
     >;
 
+    type InterruptPin = hal::gpio::Pin<
+        hal::gpio::bank0::Gpio6,
+        hal::gpio::FunctionSio<hal::gpio::SioInput>,
+        hal::gpio::PullUp,
+    >;
+
     #[shared]
     struct Shared {}
 
@@ -62,6 +68,7 @@ mod app {
     struct Local {
         uart: Uart0,
         i2c: I2c0,
+        interrupt: InterruptPin,
     }
 
     #[init]
@@ -81,8 +88,8 @@ mod app {
             &mut ctx.device.RESETS,
             &mut watchdog,
         )
-        .ok()
-        .unwrap();
+            .ok()
+            .unwrap();
 
         let sio = Sio::new(ctx.device.SIO);
 
@@ -121,12 +128,17 @@ mod app {
             ctx.device.I2C0,
             sda_pin,
             scl_pin,
-            10.kHz(),
+            400.kHz(),
             &mut ctx.device.RESETS,
             clocks.system_clock.freq(),
         );
 
         uart.write_full_blocking(b"Hello RTIC mpu6050!\r\n");
+
+        let interrupt = pins.gpio6.into_pull_up_input();
+
+        // Enable the interrupt on this specific pin
+        interrupt.set_interrupt_enabled(hal::gpio::Interrupt::EdgeHigh, true);
 
         let sensor_adr: u8 = 0x68u8;
         // Wake up MPU6050 (it starts in sleep mode)
@@ -134,10 +146,25 @@ mod app {
             Ok(_) => uart.write_full_blocking(b"Sensor wakeup OK\r\n"),
             Err(_) => uart.write_full_blocking(b"Sensor wakeup failed\r\n"),
         }
+        // Set sample rate divider - slows output to 100Hz (1000Hz / (1 + 9))
+        // Default is 1000Hz which is too fast for uart...
+        match i2c.write(sensor_adr, &[0x19, 9]) {
+            Ok(_) => uart.write_full_blocking(b"Sample rate set OK\r\n"),
+            Err(_) => uart.write_full_blocking(b"Sample rate set failed\r\n"),
+        }
+        match i2c.write(sensor_adr, &[0x38, 0x01]) {
+            Ok(_) => uart.write_full_blocking(b"Sensor interrupt enable OK\r\n"),
+            Err(_) => uart.write_full_blocking(b"Sensor interrupt enable failed\r\n"),
+        }
 
-        read_sensor::spawn().ok();
-
-        (Shared {}, Local { uart, i2c })
+        (
+            Shared {},
+            Local {
+                uart,
+                i2c,
+                interrupt,
+            },
+        )
     }
 
     #[idle]
@@ -147,42 +174,36 @@ mod app {
         }
     }
 
-    #[task(local = [uart, i2c], priority = 1)]
-    async fn read_sensor(ctx: read_sensor::Context) {
+    #[task(binds = IO_IRQ_BANK0, local = [interrupt], priority = 2)]
+    fn gpio_irq(ctx: gpio_irq::Context) {
+        if ctx.local.interrupt.interrupt_status(hal::gpio::Interrupt::EdgeHigh) {
+            ctx.local.interrupt.clear_interrupt(hal::gpio::Interrupt::EdgeHigh);
+            read_i2c::spawn().ok();
+        }
+    }
+
+    #[task(local = [i2c, uart], priority = 1)]
+    async fn read_i2c(ctx: read_i2c::Context) {
+        let sensor_adr: u8 = 0x68u8;
         let mut s: String<64> = String::new();
 
-        let sensor_adr: u8 = 0x68u8;
-
-        Mono::delay(100.millis()).await;
-
-        // Read accelerometer and gyro data
+        // Read sensor data
         let mut buffer = [0u8; 14];
-        ctx.local.uart.write_full_blocking(b"Reading sensor...\r\n");
-        loop {
-            Mono::delay(1000.millis()).await;
-            match ctx.local.i2c.write_read(sensor_adr, &[0x3B], &mut buffer) {
-                Ok(_) => {
-                    let ax = i16::from_be_bytes([buffer[0], buffer[1]]);
-                    let ay = i16::from_be_bytes([buffer[2], buffer[3]]);
-                    let az = i16::from_be_bytes([buffer[4], buffer[5]]);
-                    let gx = i16::from_be_bytes([buffer[8], buffer[9]]);
-                    let gy = i16::from_be_bytes([buffer[10], buffer[11]]);
-                    let gz = i16::from_be_bytes([buffer[12], buffer[13]]);
-                    write!(
-                        s,
-                        "Accel [{},{},{}]\r\nGyro  [{},{},{}]\r\n",
-                        ax, ay, az, gx, gy, gz
-                    )
-                        .unwrap();
-                    ctx.local.uart.write_full_blocking(s.as_bytes());
-                    s.clear();
-                }
-                Err(hal::i2c::Error::Abort(code)) => {
-                    write!(s, "Read Abort: 0x{:06X}\r\n", code).unwrap();
-                    ctx.local.uart.write_full_blocking(s.as_bytes());
-                    s.clear();
-                }
-                Err(_) => ctx.local.uart.write_full_blocking(b"Read other error\r\n"),
+        match ctx.local.i2c.write_read(sensor_adr, &[0x3B], &mut buffer) {
+            Ok(_) => {
+                let ax = i16::from_be_bytes([buffer[0], buffer[1]]);
+                let ay = i16::from_be_bytes([buffer[2], buffer[3]]);
+                let az = i16::from_be_bytes([buffer[4], buffer[5]]);
+                let gx = i16::from_be_bytes([buffer[8], buffer[9]]);
+                let gy = i16::from_be_bytes([buffer[10], buffer[11]]);
+                let gz = i16::from_be_bytes([buffer[12], buffer[13]]);
+                write!(s, "Accel [{},{},{}]\r\nGyro  [{},{},{}]\r\n",
+                       ax, ay, az, gx, gy, gz).unwrap();
+                ctx.local.uart.write_full_blocking(s.as_bytes());
+                s.clear();
+            }
+            Err(_) => {
+                ctx.local.uart.write_full_blocking(b"Read error\r\n");
             }
         }
     }
